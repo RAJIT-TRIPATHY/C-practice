0fa84660c7f0cc8c0d6b2251e8c58f8fd41aac45
RAJIT-TRIPATHY <rajittripathy5@gmail.com>
2026-09-15 10:58:38 +0530

    Add files via upload

---
+++ AreaandPermeter.c
@@ -0,0 +1,19 @@
+// #include<stdio.h>
+// int main(){
+//     int a = 5;
+//     int b = 7;
+//     int c = a + b;
+//     printf("Sum = %d", c= a+b);
+//     return 0;
+//     //Addition using Subproblems//
+//     int sum(int ,int);
+// }
+//program to calculate area and perimeter of a circle
+//program to calculate area and perimeter of a circle
+#include<stdio.h>
+#define pi 3.141
+void main(){
+    float r =2.5, area =pi*r*r,permtr=2*pi*r;
+    printf("Enter the radius value :");
+    printf("Area of circle : %f",area);
+}
